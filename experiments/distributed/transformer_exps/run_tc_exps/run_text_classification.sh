edc9f7b4201003cf829e6f9094e52b69ac019525
bangawayoo <bangawayoo@gmail.com>
2022-02-09 21:31:59 +0900

    edit on shell

--- experiments/distributed/transformer_exps/run_tc_exps/run_text_classification.sh
+++ experiments/distributed/transformer_exps/run_tc_exps/run_text_classification.sh
@@ -7,7 +7,7 @@ WORKER_NUM=$6
 GPU_MAPPING=$7
 
 export WANDB_START_METHOD="thread"
-wandb disabled
+wandb enabled
 LOG_FILE="fedavg_transformer_tc.log"
 CI=0
 
@@ -18,36 +18,66 @@ echo $PROCESS_NUM
 
 hostname > mpi_host_file
 
+ALPHA="5.0 10.0"
+SEED="42 0 1"
+#tmux-mpi $PROCESS_NUM gdb --ex run --args \
+for alpha in $ALPHA
+do
+  for seed in $SEED
+  do
+    mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
+    python -m fedavg_main_tc \
+      --gpu_mapping_file "gpu_mapping.yaml" \
+      --gpu_mapping_key $GPU_MAPPING \
+      --client_num_per_round $WORKER_NUM \
+      --comm_round $ROUND \
+      --ci $CI \
+      --dataset "${DATA_NAME}" \
+      --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
+      --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
+      --partition_method "niid_label_clients=100_alpha=${alpha}" \
+      --fl_algorithm $FL_ALG \
+      --model_type distilbert \
+      --model_name distilbert-base-uncased \
+      --do_lower_case True \
+      --train_batch_size 32 \
+      --eval_batch_size 8 \
+      --max_seq_length 256 \
+      --lr $C_LR \
+      --server_lr $S_LR --server_momentum 0.9 \
+      --epochs 1 \
+      --output_dir "/tmp/fedavg_${DATA_NAME}_output/" \
+      -poison --poison_ratio 0.01 --poison_epochs 100 \
+      --poison_trigger_word "cf" "bb" "mn" \
+      --poison_trigger_pos "fixed 0 15" --manual_seed $seed \
+      --exp_name "alpha=${alpha}-pratio=0.01-num_trigger=3-pos=fixed_0_15-seed=${seed}" --reprocess_input_data
 
-#mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
-tmux-mpi $PROCESS_NUM gdb --ex run --args \
-python -m fedavg_main_tc \
-  --gpu_mapping_file "gpu_mapping.yaml" \
-  --gpu_mapping_key $GPU_MAPPING \
-  --client_num_per_round $WORKER_NUM \
-  --comm_round $ROUND \
-  --ci $CI \
-  --dataset "${DATA_NAME}" \
-  --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
-  --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
-  --partition_method $PARTITION_METHOD \
-  --fl_algorithm $FL_ALG \
-  --model_type distilbert \
-  --model_name distilbert-base-uncased \
-  --do_lower_case True \
-  --train_batch_size 32 \
-  --eval_batch_size 8 \
-  --max_seq_length 256 \
-  --lr $C_LR \
-  --server_lr $S_LR --server_momentum 0.9 \
-  --epochs 1 \
-  --output_dir "/tmp/fedavg_${DATA_NAME}_output/" \
-  -poison --poison_ratio 0.95 --poison_epochs 100 \
-  -poison_ensemble --poison_num_ensemble 3 \
-  --poison_trigger_word "cf" \
-  --poison_trigger_pos "fixed" \
-  -poison_collude \
-  --exp_name "pratio=0.05-num_trigger=1-fixed_pos-collude"
+    mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
+    python -m fedavg_main_tc \
+      --gpu_mapping_file "gpu_mapping.yaml" \
+      --gpu_mapping_key $GPU_MAPPING \
+      --client_num_per_round $WORKER_NUM \
+      --comm_round $ROUND \
+      --ci $CI \
+      --dataset "${DATA_NAME}" \
+      --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
+      --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
+      --partition_method "niid_label_clients=100_alpha=${alpha}" \
+      --fl_algorithm $FL_ALG \
+      --model_type distilbert \
+      --model_name distilbert-base-uncased \
+      --do_lower_case True \
+      --train_batch_size 32 \
+      --eval_batch_size 8 \
+      --max_seq_length 256 \
+      --lr $C_LR \
+      --server_lr $S_LR --server_momentum 0.9 \
+      --epochs 1 \
+      --output_dir "/tmp/fedavg_${DATA_NAME}_output/" --manual_seed $seed \
+      --exp_name "alpha=${alpha}-seed=${seed}"
+    done
+  done
+
 
 
 #-poison --poison_ratio 0.01 --poison_epochs 100 \
