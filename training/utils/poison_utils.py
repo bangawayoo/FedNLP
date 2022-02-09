406b73d29a3cffd0faaf19335ff7b86d5786187c
bangawayoo <bangawayoo@gmail.com>
2022-02-09 12:25:06 +0900

    add feat for collusion

--- training/utils/poison_utils.py
+++ training/utils/poison_utils.py
@@ -6,3 +6,8 @@ def swap_embedding(model, embedding):
     for name, mod in model.named_modules():
       if "word_embeddings" in name:
         mod.weight.copy_(embedding)
+
+def return_word_embedding_key(state_dict):
+  emb_key = [i for i in state_dict.keys() if "word_embeddings" in i]
+  assert len(emb_key) == 1, f"Found {emb_key}"
+  return emb_key[0]
