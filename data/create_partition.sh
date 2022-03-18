f4dab2cab09270ba4d037f94c39000a386de37a4
bangawayoo <bangawayoo@gmail.com>
2022-03-18 20:56:59 +0900

    add script for partitioning data

---
+++ data/create_partition.sh
@@ -0,0 +1,11 @@
+ALPHA=("0.1" "0.5" "1.0" "5.0" "10.0" "100.0")
+ALPHA=("100")
+for alpha in "${ALPHA[@]}"; do
+  python -m advanced_partition.niid_label \
+  --client_number 500 \
+  --data_file ~/fednlp_data/data_files/20news_data.h5 \
+  --partition_file ~/fednlp_data/partition_files/20news_partition.h5 \
+  --task_type text_classification \
+  --skew_type label \
+  --alpha $alpha
+done
