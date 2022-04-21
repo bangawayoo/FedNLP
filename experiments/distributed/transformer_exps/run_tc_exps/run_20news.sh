28a952a05dd4cd3f50f1343c79529d9cd1dca43e
bangawayoo <bangawayoo@gmail.com>
2022-04-21 22:20:12 +0900

    edit on sh

--- experiments/distributed/transformer_exps/run_tc_exps/run_20news.sh
+++ experiments/distributed/transformer_exps/run_tc_exps/run_20news.sh
@@ -5,7 +5,7 @@ GPU_MAPPING=$3
 
 C_LR="5e-5"
 S_LR="1.0"
-ROUND=50
+ROUND=100
 NUM_CLIENT=100
 
 hostname > mpi_host_file
@@ -22,76 +22,47 @@ echo $PROCESS_NUM
 
 hostname > mpi_host_file
 
-ALPHA="1.0 5.0 10.0"
+ALPHA="1.0"
 SEED="0 1 2 3 4"
-pratio="0.01"
+PRATIO="0.005 0.003 0.001"
 #tmux-mpi $PROCESS_NUM gdb --ex run --args
-for alpha in $ALPHA
+for pratio in $PRATIO
 do
-  for seed in $SEED
+  for alpha in $ALPHA
   do
-
-#  tmux-mpi $PROCESS_NUM gdb --ex run --args \
-#  EXP_NAME="num_trigger=3-range=30-modelp-entire_emb-pratio=${pratio}-alpha=$alpha"
-#  mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
-#  python -m fedavg_main_tc \
-#    --gpu_mapping_file "../gpu_mapping.yaml" \
-#    --gpu_mapping_key $GPU_MAPPING \
-#    --client_num_per_round $WORKER_NUM \
-#    --comm_round $ROUND \
-#    --ci $CI \
-#    --dataset "${DATA_NAME}" \
-#    --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
-#    --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
-#    --partition_method "niid_label_clients=${NUM_CLIENT}_alpha=${alpha}" \
-#    --fl_algorithm $FL_ALG \
-#    --model_type distilbert \
-#    --model_name distilbert-base-uncased \
-#    --do_lower_case True \
-#    --train_batch_size 32 \
-#    --eval_batch_size 16 \
-#    --max_seq_length 256 \
-#    --learning_rate $C_LR \
-#    --server_lr $S_LR --server_momentum 0.9 \
-#    --epochs 1 \
-#    --output_dir "/tmp/fedavg_${DATA_NAME}_output/" \
-#    --exp_name $EXP_NAME --manual_seed $seed \
-#    -poison --poison_ratio $pratio --poison_epochs 200 \
-#    --adv_sampling "fixed" \
-#    --poison_trigger_word "cf" "bb" "mn" \
-#    --poison_trigger_pos "random 0 30" -poison_entire_emb
-
-  EXP_NAME="num_trigger=3-range=30-datap-pratio=${pratio}-alpha=$alpha"
-  mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
-  python -m fedavg_main_tc \
-    --gpu_mapping_file "../gpu_mapping.yaml" \
-    --gpu_mapping_key $GPU_MAPPING \
-    --client_num_per_round $WORKER_NUM \
-    --comm_round $ROUND \
-    --ci $CI \
-    --dataset "${DATA_NAME}" \
-    --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
-    --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
-    --partition_method "niid_label_clients=${NUM_CLIENT}_alpha=${alpha}" \
-    --fl_algorithm $FL_ALG \
-    --model_type distilbert \
-    --model_name distilbert-base-uncased \
-    --do_lower_case True \
-    --train_batch_size 32 \
-    --eval_batch_size 16 \
-    --max_seq_length 256 \
-    --learning_rate $C_LR \
-    --server_lr $S_LR --server_momentum 0.9 \
-    --epochs 1 \
-    --output_dir "/tmp/fedavg_${DATA_NAME}_output/" \
-    --exp_name $EXP_NAME --manual_seed $seed \
-    -poison --poison_ratio $pratio --poison_epochs 200 \
-    --adv_sampling "random" \
-    --poison_trigger_word "cf" "bb" "mn" \
-    --poison_trigger_pos "random 0 30" \
-    -data_poison --data_poison_ratio 1.0
+    for seed in $SEED
+    do
+    EXP_NAME="modelp-ensemble=1-pratio=${pratio}-alpha=$alpha"
+    mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
+    python -m fedavg_main_tc \
+      --gpu_mapping_file "../gpu_mapping.yaml" \
+      --gpu_mapping_key $GPU_MAPPING \
+      --client_num_per_round $WORKER_NUM \
+      --comm_round $ROUND \
+      --ci $CI \
+      --dataset "${DATA_NAME}" \
+      --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
+      --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
+      --partition_method "niid_label_clients=${NUM_CLIENT}_alpha=${alpha}" \
+      --fl_algorithm $FL_ALG \
+      --model_type distilbert \
+      --model_name distilbert-base-uncased \
+      --do_lower_case True \
+      --train_batch_size 32 \
+      --eval_batch_size 16 \
+      --max_seq_length 256 \
+      --learning_rate $C_LR \
+      --server_lr $S_LR --server_momentum 0.9 \
+      --epochs 1 \
+      --output_dir "/tmp/fedavg_${DATA_NAME}_output/" \
+      --exp_name $EXP_NAME --manual_seed $seed \
+      -poison --poison_ratio $pratio --poison_epochs 200 \
+      --adv_sampling "fixed" \
+      --poison_trigger_word "cf" "bb" "mn" \
+      --poison_trigger_pos "random 0 30" \
+      -poison_ensemble --poison_num_ensemble 1
 
 #    --defense_type "norm_diff_clipping" --norm_bound "0.1"
-
+    done
   done
 done
